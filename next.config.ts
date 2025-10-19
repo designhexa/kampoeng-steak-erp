595035d072c9b422ab7c2a27bb53658f6b2b62f7
designhexa <157108416+designhexa@users.noreply.github.com>
2025-10-19 12:18:18 +0700

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,18 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   // Use a custom build directory to avoid issues with a locked `.next` folder
   distDir: '.next',
-  output: 'standalone'
+  output: 'standalone',
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        ws: false,
+        'utf-8-validate': false,
+        'bufferutil': false
+      };
+    }
+    return config;
+  }
 };
 
 export default nextConfig;
