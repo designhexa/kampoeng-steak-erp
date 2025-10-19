93cff83cf1cbe34419c283ed0a7a35e9b1afa355
designhexa <157108416+designhexa@users.noreply.github.com>
2025-10-19 12:19:45 +0700

    Add files via upload

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,8 @@
 import { createServerClient, type CookieOptions } from "@supabase/ssr"
 import { type NextRequest, NextResponse } from "next/server"
 
+export const runtime = 'edge'
+
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
