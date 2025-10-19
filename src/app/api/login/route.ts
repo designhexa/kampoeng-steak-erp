27126f4d0405d3323f88b492f7e0223813fa3d7a
designhexa <157108416+designhexa@users.noreply.github.com>
2025-10-19 11:09:44 +0700

    Add files via upload

---
+++ src/app/api/login/route.ts
@@ -0,0 +1,42 @@
+import { createServerClient, type CookieOptions } from '@supabase/ssr';
+import { type NextRequest, NextResponse } from 'next/server';
+
+export async function POST(request: NextRequest) {
+  const requestUrl = new URL(request.url);
+  const response = NextResponse.next();
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return request.cookies.get(name)?.value;
+        },
+        set(name: string, value: string, options: CookieOptions) {
+          response.cookies.set({
+            name,
+            value,
+            ...options,
+          });
+        },
+        remove(name: string, options: CookieOptions) {
+          response.cookies.delete({
+            name,
+            ...options,
+          });
+        },
+      },
+    }
+  );
+
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email: "admin@kampoengsteak.com",
+    password: "admin123",
+  });
+
+  if (error) {
+    return NextResponse.json({ error: error.message }, { status: 400 });
+  }
+
+  return NextResponse.json(data);
+}
