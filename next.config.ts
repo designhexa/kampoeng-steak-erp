a471e66f763d20357581831759d8ac10b343f3fb
designhexa <157108416+designhexa@users.noreply.github.com>
2025-10-19 11:01:26 +0700

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // Use a custom build directory to avoid issues with a locked `.next` folder
-  distDir: 'build',
+  distDir: '.next',
+  output: 'standalone'
 };
 
 export default nextConfig;
